51d35e815ba294a65c0bc6eb5b6fac25db8b8d73
unknown <patrick@shimi.co.uk>
2017-04-19 14:42:57 +0100

    updates for me

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 Webcam-Face-Detect
 ==================
 
+pip install opencv-python
+pip install opencv-contrib-python
+
 Run the program like this:
 
-python webcam.py haarcascade_frontalface_default.xml
+python webcam_cv3.py haarcascade_frontalface_default.xml
 
 If you want to understand how the code works,  see here: https://realpython.com/blog/python/face-detection-in-python-using-a-webcam/
-
-
-Update: Now supports OpenCV3. This change has been made by furetosan ( https://github.com/furetosan) and tested on Linux.
-
-To run the OpenCV3 version, run python webcam_cv3.py haarcascade_frontalface_default.xml
-
--- webcam_cv3.py
+++ webcam_cv3.py
@@ -1,26 +1,12 @@
 import cv2
 import sys
 import logging as log
-import datetime as dt
+import numpy as np
 
 from time import sleep
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
-log.basicConfig(filename='webcam.log',level=log.INFO)
 
-video_capture = cv2.VideoCapture(0)
-anterior = 0
-
-while True:
-    if not video_capture.isOpened():
-        print('Unable to load camera.')
-        sleep(5)
-        pass
-
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-
+def face_detect(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(
@@ -32,15 +18,60 @@ while True:
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    if anterior != len(faces):
-        anterior = len(faces)
-        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+class HandTrack():
+
+    def __init__(self):
+        self.fgmask = None
+        # self.fgbg = cv2.bgsegm.createBackgroundSubtractor()
+        self.fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+        # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        # fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
+
+    def get_background(self, frame):
+        if self.fgmask is None:
+            self.fgmask = self.fgbg.apply(frame)
+            print('Got background')
+    #    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    def remove_bg(self, frame):
+        if self.fgmask is not None:
+            fg_mask = self.fgmask.apply(frame)
+            kernel = np.ones((3, 3), np.uint8)
+            fg_mask = cv2.erode(fg_mask, kernel, iterations=1)
+            frame = cv2.bitwise_and(frame, frame, mask=fg_mask)
+        return frame
+
+
+cascPath = sys.argv[1]
+faceCascade = cv2.CascadeClassifier(cascPath)
+log.basicConfig(filename='webcam.log', level=log.INFO)
+
+print(cv2.__version__)
+hand = HandTrack()
+bg = cv2.bgsegm.createBackgroundSubtractorMOG()
+video_capture = cv2.VideoCapture(0)
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    # face_detect(frame)
+    frame = hand.remove_bg(frame)
+
+    keypress = cv2.waitKey(1)
+    if keypress & 0xFF == ord('q'):
         break
+    if keypress & 0xFF == ord('b'):
+        hand.get_background(frame)
 
+    face_detect(frame)
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
